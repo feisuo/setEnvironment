4c42bd0cd57d920085bb4789ef69e2e8f538e530
kai  liu <feisuo.liu@gmail.com>
2017-11-11 12:10:54 +0800

    Update setJavaLinux.sh

--- src/setJavaLinux.sh
+++ src/setJavaLinux.sh
@@ -1,2 +1,2 @@
-# downoad java
+# download java
 curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz > jdk.tar.gz
